f00ffdde6e98a233270215c27ae6e5b301a09bec
OlenaKovalenko <131033072+OlenaKovalenko@users.noreply.github.com>
2023-12-06 15:09:33 +0200

    Initial commit

---
+++ src/components/ContactItem/ContactItem.js
@@ -0,0 +1,14 @@
+import { useDispatch } from "react-redux"
+import { BtnDelete, ContactLi, ContactName } from "./ContactItem.styled"
+import { deleteContact } from "redux/contactsSlice";
+
+export const ContactItem = ({ contact: {name, number, id}}) => {
+    const dispatch = useDispatch();
+
+    return (
+        <ContactLi>
+            <ContactName>{name}: {number}</ContactName>
+            <BtnDelete type="button" onClick={() => dispatch(deleteContact(id))}>Delete</BtnDelete>
+        </ContactLi>
+    )
+}
